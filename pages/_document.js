3de0c20bf83aafe55d9a168c70175d3c782758fe
Lang Xie <admin@saintex.co.uk>
2021-07-23 12:33:42 +0100

    init

--- pages/_document.js
+++ pages/_document.js
@@ -8,8 +8,8 @@ class MyDocument extends Document {
 
   render() {
     const meta = {
-      title: 'Next.js Blog Starter Kit',
-      description: 'Clone and deploy your own Next.js portfolio in minutes.',
+      title: 'Expose faizasaqlain',
+      description: 'Expose this established company that preys on people in need of Pakistani luxury fashion',
       image:
         'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
